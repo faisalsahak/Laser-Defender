3c6d4710bb3f37709b55c56f8d71ef63995bd78c
user <user@users-MacBook-Pro.local>
2019-05-28 14:59:57 -0700

    added new paths for the enemy ships to move on, added a script to dynamically generate enemy ships and move them in a certain path

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
 
-	[SerializeField] WaveConfig waveConfig;
+	WaveConfig waveConfig;
 	List<Transform> waypoints;
-	[SerializeField] float moveSpeed = 2f;
+	// [SerializeField] float moveSpeed = 2f;
 	int waypointIndex = 0;
     // Start is called before the first frame update
     void Start()
@@ -24,10 +24,14 @@ public class EnemyPathing : MonoBehaviour
         
     }
 
+    public void SetWaveConfig(WaveConfig waveConfig){
+    	this.waveConfig = waveConfig;
+    }
+
     private void Move(){
     	if(waypointIndex< waypoints.Count){ //moves the space ship from one way point to another
     		var targetPosition = waypoints[waypointIndex].transform.position;
-    		var movementThisFrame = moveSpeed * Time.deltaTime;
+    		var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
     		transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
     		if(transform.position == targetPosition)
     			waypointIndex++;
