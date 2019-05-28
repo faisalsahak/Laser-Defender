a1685b76da589a1683cf81dedac50cc64fb798bc
user <user@users-MacBook-Pro.local>
2019-05-28 15:13:22 -0700

    added multiple enemy ships in a wave, multiple waves are coming to the screen at once

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,12 +6,19 @@ public class EnemySpawner : MonoBehaviour
 {
 
 	[SerializeField] List<WaveConfig> waveConfigs;
-	int startingWave = 0;
+	[SerializeField] int startingWave = 0;
     // Start is called before the first frame update
     void Start()
     {
-      var currentWave = waveConfigs[startingWave];
-      StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+      // var currentWave = waveConfigs[startingWave];
+      StartCoroutine(SpawnAllWaves());
+    }
+
+    private IEnumerator SpawnAllWaves(){
+    	for(int i = startingWave; i<waveConfigs.Count; i++){
+    		var currentWave = waveConfigs[i];
+    		yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    	}
     }
 
     private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig){
